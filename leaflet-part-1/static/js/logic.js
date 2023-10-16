7b648a6a9cf5671c8c9b6db310afe26ce82c99f8
angjoagos <127690253+angjoagos@users.noreply.github.com>
2023-10-16 18:28:06 -0500

    update

---
+++ leaflet-part-1/static/js/logic.js
@@ -0,0 +1,5 @@
+// Create the tile layer that will be the background of our map.
+let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
